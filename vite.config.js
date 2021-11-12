525b4db28f00b01d0f1db692915aca34df08c397
Ryan Carniato <ryansolid@gmail.com>
2021-11-11 20:18:14 -0800

    update to the latest

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import solid from "solid-start";
-import vercel from "solid-start-vercel-edge";
+import vercel from "solid-start-vercel";
 
 export default defineConfig({
   plugins: [solid({ adapter: vercel() })],
