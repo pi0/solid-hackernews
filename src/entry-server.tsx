4c8ee43b5e02a423a03f1dd86e94e879b259aefc
Ryan Carniato <ryansolid@gmail.com>
2022-03-27 20:47:14 -0700

    update to latest

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -1,15 +1,3 @@
-import { renderToStream } from "solid-js/web";
-import { RequestContext, StartServer } from "solid-start/components";
+import { StartServer, renderStream } from "solid-start/entry-server";
 
-export default async (context: RequestContext) => {
-  // streaming
-  const { readable, writable } = new TransformStream();
-  renderToStream(() => <StartServer context={context} />).pipeTo(writable);
-
-  context.responseHeaders.set("Content-Type", "text/html");
-
-  return new Response(readable, {
-    status: 200,
-    headers: context.responseHeaders,
-  });
-};
+export default renderStream((context) => <StartServer context={context} />)();
