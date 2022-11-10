78f15cbcd059c831acfb3af8430ababf08aaab07
Ryan Carniato <ryansolid@gmail.com>
2022-11-09 21:25:36 -0800

    make pages example

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import solid from "solid-start/vite";
-import cloudflare from "solid-start-cloudflare-workers";
+import cloudflare from "solid-start-cloudflare-pages";
 
 export default defineConfig({
   plugins: [solid({ adapter: cloudflare() })]
